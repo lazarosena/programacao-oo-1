f27ac83a160b8257c161e237b5ed415735a094dd
Vinicius Chruczeski <150845487+IKSEZ@users.noreply.github.com>
2025-04-10 17:30:07 +0000

    placar

--- EstudoDirigido1/placar.java
+++ EstudoDirigido1/placar.java
@@ -1,4 +1,10 @@
 public class Placar
 {
     public static void exibirPlacar(Jogador Jogador)
+    System.out.println("======PLACAR FINAL======");
+    System.out.println("Jogador: " + jogador.getNome());
+    System.out.println("Tentativas: " + jogador.getTentativas());
+    System.out.println("Pontuação: " + jogador.getPontuacao());
+    System.out.println("========================");
+
 }
