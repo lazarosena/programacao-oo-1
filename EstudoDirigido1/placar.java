5a48651ccdbd1713e1d8c110a3ed1d46f26484c7
Vinicius Chruczeski <150845487+IKSEZ@users.noreply.github.com>
2025-04-10 18:02:37 +0000

    ajuste placar

--- EstudoDirigido1/placar.java
+++ EstudoDirigido1/placar.java
@@ -1,6 +1,33 @@
+import java.util.ArrayList;
+
 public class Placar
 {
-    public static void exibirPlacar(Jogador Jogador)
+    private ArrayList<Jogador> ranking; 
+
+    //construtor
+    public Placar()
+    {
+        this.ranking = new ArrayList<>();
+    }
+
+    //adiciona no ranking
+    public void adicionaJogadorRanking(jogador j)
+    {
+        this.ranking.add(j);
+    }
+
+    public void mostraRankingTela()
+    {
+        System.out.println("\n===== RANKING DE JOGADORES =====");
+        for (jogador j : ranking)
+        {
+            System.out.println("Jogador: " + j.getNome() + "| Pontuaçao: " + j.getPontuacao());
+        }
+        System.out.println("================================");
+    }
+
+
+    public void exibirPlacarFinal(Jogador Jogador)
     System.out.println("======PLACAR FINAL======");
     System.out.println("Jogador: " + jogador.getNome());
     System.out.println("Tentativas: " + jogador.getTentativas());
